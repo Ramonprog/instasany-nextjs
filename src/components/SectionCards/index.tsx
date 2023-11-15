520a0ec101f08e37e7a71d28dc65c3770b154df6
Ramonprog <programador.ramon@gmail.com>
2023-11-15 08:12:14 -0300

    feat: primeira parte da animação

--- src/components/SectionCards/index.tsx
+++ src/components/SectionCards/index.tsx
@@ -1,5 +1,6 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Mockup from '/public/mockup-main.png'
 import Card01 from '/public/image-01.png'
 import Card02 from '/public/image-02.png'
@@ -7,17 +8,54 @@ import Card03 from '/public/image-03.png'
 import Card04 from '/public/image-04.png'
 import Card05 from '/public/image-05.png'
 import Card06 from '/public/image-06.png'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const SectionCards = () => {
+
+    const mockupRef = useRef(null)
+    const titleRef = useRef(null)
+
+
+    useEffect(() => {
+        const mPhone = mockupRef.current
+        const title = titleRef.current
+
+        //animação do mockup
+        gsap.fromTo(mPhone, {
+            opacity: 0,
+            scale: .5
+        }, {
+            opacity: 1,
+            scale: 1,
+            duration: 1,
+            ease: 'power4.out'
+        })
+
+        //animação do titulo
+        gsap.fromTo(title, {
+            opacity: 0,
+            y: 100
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+
+        })
+    }, [])
+
     return (
         <section className='w-full mx-auto pb-20'>
             <Image
                 src={Mockup}
                 alt='mockup celular com foto de uma garota'
-                className='sticky top-56 mx-auto -mt-[32rem] mb-16 z-10'
+                className='sticky top-56 mx-auto -mt-[32rem] mb-16 z-10 opacity-0'
+                ref={mockupRef}
             />
 
-            <h2 className='text-center text-7xl font-semibold mb-56'>Faça <span className='text-[#719d96]'>voçe</span> mesmo de casa</h2>
+            <h2 className='text-center text-7xl font-semibold mb-56 opacity-0' ref={titleRef}>Faça <span className='text-[#719d96]'>voçe</span> mesmo de casa</h2>
 
             <div className='w-full max-w-[1320px] mx-auto h-[562px] relative'>
                 <Image
--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -1,15 +1,55 @@
+'use client'
 import Image from "next/image"
+import { useEffect, useRef } from "react"
 import GridContainer from "../Grid"
 import IconFile from '/public/icon-file.svg'
 import IconHandOne from '/public/icon-hand.svg'
 import IconHandTwo from '/public/icon-hand-02.svg'
 import Mockup from '/public/mockup.svg'
+import gsap from "gsap"
 
 const SectionHero = () => {
+    const textHeroRef = useRef(null)
+    const mockupLeftRef = useRef(null)
+    const mockupRightRef = useRef(null)
+
+    useEffect(() => {
+        const textHero = textHeroRef.current
+        const mLeft = mockupLeftRef.current
+        const mRight = mockupRightRef.current
+
+        //animação do texto
+        gsap.fromTo(textHero, {
+            opacity: 0,
+            y: 20
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: .8,
+            ease: 'powerr.out'
+        })
+
+        //animação mockup left
+        gsap.fromTo(mLeft, {
+            rotate: 0
+        }, {
+            rotate: -12,
+            duration: 1
+        })
+
+        //animação mockup right
+        gsap.fromTo(mRight, {
+            rotate: 0
+        }, {
+            rotate: 12,
+            duration: 1
+        })
+    }, [])
+
     return (
         <section className="relative w-full bg-hero bg-no-repeat bg-center bg-top bg-green-primary h-[834px] pt-16 overflow-hidden">
             <GridContainer className="flex flex-col items-center">
-                <div className="w-full max-w-[1056px] text-center">
+                <div className="w-full max-w-[1056px] text-center opacity-0" ref={textHeroRef}>
                     <h3 className="text-xl font-medium  text-teal-800 mb-4">Novo curso focado em instagram</h3>
                     <h1 className="text-white text-7xl/normal font-semibold mb-2">Destrave as suas habilidades</h1>
                     <div className="flex items-center justify-center gap-10">
@@ -42,12 +82,14 @@ const SectionHero = () => {
                     <Image
                         src={Mockup}
                         alt="Mockup tela de celular"
-                        className="-rotate-12 relative top-5 left-14"
+                        className="- relative top-5 left-14"
+                        ref={mockupLeftRef}
                     />
                     <Image
                         src={Mockup}
                         alt="Mockup tela de celular"
-                        className="rotate-12 relative top-5 right-14"
+                        className=" relative top-5 right-14"
+                        ref={mockupRightRef}
                     />
                 </div>
             </GridContainer>
